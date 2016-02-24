87cc6226075e36e3af3fed49bbd7bf6a2f753274
emerge <i.i.babko@gmail.com>
2016-02-24 14:41:14 +0200

    install bootstrap and jquery, update gulpfile, add default navbar to main page

--- gulpfile.js
+++ gulpfile.js
@@ -13,4 +13,6 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss');
+    mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/jquery.min.js');
+    mix.copy('node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js', 'public/js/bootstrap.min.js');
 });
