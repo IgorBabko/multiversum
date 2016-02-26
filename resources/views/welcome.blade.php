68971389a4aacfa316f6b56f8f32c041ab845753
emerge <i.i.babko@gmail.com>
2016-02-26 23:24:38 +0200

    add responsiveness to footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -214,9 +214,8 @@
         <footer>
             <div class="container">
                 <div class="row">
-                    <div class="col-xs-3 item"><i class="fa fa-home"></i>Наше расположение</div>
-                    <div class="col-xs-3 item"><i class="fa fa-envelope"></i><a id="email-link" href="mailto:portaciya@gmail.com">portaciya@gmail.com</a></div>
-                    <div class="col-xs-3 item">
+                    <div class="col-xs-12 col-sm-6 col-md-4 item"><i class="fa fa-home"></i>Наше расположение</div>
+                    <div class="col-xs-12 col-sm-6 col-md-4 item">
                         <ul class="social-icons-list">
                             <li><i class="fa fa-facebook"></i></li>
                             <li><i class="fa fa-instagram"></i></li>
@@ -224,7 +223,7 @@
                             <li><i class="fa fa-google-plus"></i></li>
                         </ul>
                     </div>
-                    <div class="col-xs-3 item">
+                    <div class="col-xs-12 col-sm-12 col-md-4 item">
                         <span class="logo">Multiversum</span><br/>
                         <span id="copyright">&copy; Все права защищены</span>
                     </div>
