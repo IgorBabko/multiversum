c7eee530b22f14d0e502999c8137452295a560fa
emerge <i.i.babko@gmail.com>
2016-02-28 17:57:01 +0200

    add search on webinars page

--- resources/views/pages/webinars.blade.php
+++ resources/views/pages/webinars.blade.php
@@ -1,93 +1,100 @@
 @extends("layout")
-
 @section("content")
-    <div class="video-content video-block-wrapper">
-        <div class="container video-block">
-            <div class="row centered">
+<div class="video-content video-block-wrapper">
+    <div class="container video-block">
+        <div class="row">
+            <div class="col-md-12">
                 <h1>Вебинары</h1>
-                <hr class="light-line">
-                <div class="col-md-4 col-sm-6">
-                    <div class="video">
-                        <img src="http://placehold.it/300x250">
-                        <span class="caption">
-                            <i class="fa fa-play"></i>
-                        </span>
-                    </div>
-                    <h3>Видео 1</h3>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="video">
-                        <img src="http://placehold.it/300x250">
-                        <span class="caption">
-                            <i class="fa fa-play-circle"></i>
-                        </span>
-                    </div>
-                    <h3>Видео 2</h3>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="video">
-                        <img src="http://placehold.it/300x250">
-                        <span class="caption">
-                            <i class="fa fa-play-circle-o"></i>
-                        </span>
-                    </div>
-                    <h3>Видео 3</h3>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="video">
-                        <img src="http://placehold.it/300x250">
-                        <span class="caption">
-                            <i class="fa fa-play"></i>
-                        </span>
-                    </div>
-                    <h3>Видео 1</h3>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="video">
-                        <img src="http://placehold.it/300x250">
-                        <span class="caption">
-                            <i class="fa fa-play-circle"></i>
-                        </span>
-                    </div>
-                    <h3>Видео 2</h3>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="video">
-                        <img src="http://placehold.it/300x250">
-                        <span class="caption">
-                            <i class="fa fa-play-circle-o"></i>
-                        </span>
-                    </div>
-                    <h3>Видео 3</h3>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="video">
-                        <img src="http://placehold.it/300x250">
-                        <span class="caption">
-                            <i class="fa fa-play"></i>
-                        </span>
-                    </div>
-                    <h3>Видео 1</h3>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="video">
-                        <img src="http://placehold.it/300x250">
-                        <span class="caption">
-                            <i class="fa fa-play-circle"></i>
-                        </span>
-                    </div>
-                    <h3>Видео 2</h3>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="video">
-                        <img src="http://placehold.it/300x250">
-                        <span class="caption">
-                            <i class="fa fa-play-circle-o"></i>
-                        </span>
-                    </div>
-                    <h3>Видео 3</h3>
+                <!-- <hr class="light-line"> -->
+                <div class="search">
+                    <input type="text" class="search" placeholder="поиск">
+                    <i class="fa fa-search"></i>
                 </div>
             </div>
         </div>
+        <div class="row centered">
+            <div class="col-md-4 col-sm-6">
+                <div class="video">
+                    <img src="http://placehold.it/300x250">
+                    <span class="caption">
+                        <i class="fa fa-play"></i>
+                    </span>
+                </div>
+                <h3>Видео 1</h3>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="video">
+                    <img src="http://placehold.it/300x250">
+                    <span class="caption">
+                        <i class="fa fa-play-circle"></i>
+                    </span>
+                </div>
+                <h3>Видео 2</h3>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="video">
+                    <img src="http://placehold.it/300x250">
+                    <span class="caption">
+                        <i class="fa fa-play-circle-o"></i>
+                    </span>
+                </div>
+                <h3>Видео 3</h3>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="video">
+                    <img src="http://placehold.it/300x250">
+                    <span class="caption">
+                        <i class="fa fa-play"></i>
+                    </span>
+                </div>
+                <h3>Видео 1</h3>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="video">
+                    <img src="http://placehold.it/300x250">
+                    <span class="caption">
+                        <i class="fa fa-play-circle"></i>
+                    </span>
+                </div>
+                <h3>Видео 2</h3>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="video">
+                    <img src="http://placehold.it/300x250">
+                    <span class="caption">
+                        <i class="fa fa-play-circle-o"></i>
+                    </span>
+                </div>
+                <h3>Видео 3</h3>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="video">
+                    <img src="http://placehold.it/300x250">
+                    <span class="caption">
+                        <i class="fa fa-play"></i>
+                    </span>
+                </div>
+                <h3>Видео 1</h3>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="video">
+                    <img src="http://placehold.it/300x250">
+                    <span class="caption">
+                        <i class="fa fa-play-circle"></i>
+                    </span>
+                </div>
+                <h3>Видео 2</h3>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="video">
+                    <img src="http://placehold.it/300x250">
+                    <span class="caption">
+                        <i class="fa fa-play-circle-o"></i>
+                    </span>
+                </div>
+                <h3>Видео 3</h3>
+            </div>
+        </div>
     </div>
+</div>
 @endsection
