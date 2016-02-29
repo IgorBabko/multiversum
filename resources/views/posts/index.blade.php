5cd3f865d3db717ba97f5868e594ec89dfd74a48
emerge <i.i.babko@gmail.com>
2016-02-29 17:44:01 +0200

    add bootstrap-select dependency

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,39 @@
+@extends("layout")
+@section("content")
+<div class="video-content video-block-wrapper">
+    <div class="container video-block">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Вебинары</h1>
+                <!-- <hr class="light-line"> -->
+                <div class="search">
+                    <input type="text" class="search" placeholder="поиск">
+                    <i class="fa fa-search"></i>
+                </div>
+            </div>
+        </div>
+        <div class="row centered">
+            @forelse ($webinars as $webinar)
+            <div class="col-md-4 col-sm-6">
+                <div class="video">
+                    <img src="http://placehold.it/300x250">
+                    <div class="caption">
+                        <div>{{ $webinar->description }}</div>
+                        <i class="fa fa-play"></i>
+                    </div>
+                </div>
+                <div class="meta-info">
+                    <span class="name">{{ $webinar->name }}</span>
+                    <span class="date">{{ $webinar->date->format('m.d.Y') }}</span>
+                </div>
+            </div>
+            @empty
+            Вебинаров нет
+            @endforelse
+            <div class="col-xs-12 pagination-block">
+                {!! $webinars->links() !!}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
