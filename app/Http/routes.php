556d95b8d1f8ba8efba540ef9a0045e212336306
emerge <i.i.babko@gmail.com>
2016-03-07 19:56:57 +0200

    WIP

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,18 +22,20 @@
 |
  */
 
-Route::group(['middleware' => ['web']], function () {
-    Route::get('/', ['as' => 'index', 'uses' => 'PagesController@index']);
+Route::get('/', ['as' => 'index', 'uses' => 'PagesController@index']);
 
-    Route::get('/webinars', ['as' => 'webinars', 'uses' => 'WebinarsController@index']);
-    Route::get('/webinars/{slug}', ['as' => 'webinar', 'uses' => 'WebinarsController@showWebinar']);
+Route::get('/webinars', ['as' => 'webinars', 'uses' => 'WebinarsController@index']);
+Route::get('/webinars/{slug}', ['as' => 'webinar', 'uses' => 'WebinarsController@showWebinar']);
 
-    Route::get('/books', ['as' => 'books', 'uses' => 'BooksController@index']);
-    Route::get('/books/{slug}', ['as' => 'book', 'uses' => 'BooksController@showBook']);
+Route::get('/books', ['as' => 'books', 'uses' => 'BooksController@index']);
+Route::get('/books/{slug}', ['as' => 'book', 'uses' => 'BooksController@showBook']);
 
-    Route::get('/disks', ['as' => 'disks', 'uses' => 'DisksController@index']);
-    Route::get('/disks/{slug}', ['as' => 'disk', 'uses' => 'DisksController@showDisk']);
+Route::get('/disks', ['as' => 'disks', 'uses' => 'DisksController@index']);
+Route::get('/disks/{slug}', ['as' => 'disk', 'uses' => 'DisksController@showDisk']);
 
-    Route::get('/posts', ['as' => 'posts', 'uses' => 'PostsController@index']);
-    Route::get('/posts/{slug}', ['as' => 'post', 'uses' => 'PostsController@showPost']);
-});
+Route::get('/posts', ['as' => 'posts', 'uses' => 'PostsController@index']);
+Route::get('/posts/{slug}', ['as' => 'post', 'uses' => 'PostsController@showPost']);
+
+// Route::group(['middleware' => ['web']], function () {
+
+// });
