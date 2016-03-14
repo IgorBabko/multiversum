b37da0433a2ac2c2e66c1cf79225cc1efcbe549d
emerge <i.i.babko@gmail.com>
2016-03-14 10:53:48 +0200

    clear dashboard.js file from debug output

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -47,21 +47,15 @@ $(function() {
         }]
     });
 
-    console.log(location);
-
     $('.delete').on('click', function () {
-        console.log($(this).closest('tr').find('td:first-child').text());
         $('.delete-form').attr('action', location.pathname + '/' + $(this).closest('tr').find('td:first-child').text() );
     });
 
     $('thead').on('click', function () {
-        console.log('clcked thead');
         setTimeout(function () {
             $('.delete').off().on('click', function () {
-                console.log($(this).closest('tr').find('td:first-child').text());
                 $('.delete-form').attr('action', location.pathname + '/' + $(this).closest('tr').find('td:first-child').text() );
             });
-            console.log("timeout");
         }, 100);
     });
 });
