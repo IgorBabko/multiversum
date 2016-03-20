6ee030bf5826a7e5b0cb4e73c4899b33d648a1f2
emerge <i.i.babko@gmail.com>
2016-03-20 15:57:10 +0200

    add thumbnails to videos from youtube

--- app/Webinar.php
+++ app/Webinar.php
@@ -22,6 +22,11 @@ class Webinar extends Model implements SluggableInterface
 
     public function getIframeAttribute()
     {
-        return Embed::make($this->url)->parseUrl()->getHtml();
+        return Embed::make('https://youtu.be/' . $this->youtube_id)->parseUrl()->getHtml();
+    }
+
+    public function getThumbnailAttribute()
+    {
+        return 'http://img.youtube.com/vi/' . $this->youtube_id . '/0.jpg';
     }
 }
