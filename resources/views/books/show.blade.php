42fac67c7a884f3cc3f996b376bd25b38ae4a5dc
emerge <i.i.babko@gmail.com>
2016-03-20 17:17:04 +0200

    improve book page styles

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,44 +1,15 @@
 @extends("layout")
 @section("content")
-<div class="book-content page-content books-wrapper">
-    <div class="container">
-        <div class="row">
-            <h1>{{ $book->name }}</h1>
-            <hr class="light-line">
-            <div class="book-wrapper col-xs-12 col-sm-4 col-md-3">
-                <img src="http://placehold.it/200x270" alt="Книга 1">
-                <button class="btn buy-button">Купить</button>
-            </div>
-            <div class="col-xs-12 col-sm-8 col-md-9 description-block">
-                <p class="description">{!! $book->description !!}</p>
-                <span>Дата публикации: {{ $book->published_at->format('m.d.Y') }}</span>
-            </div>
-        </div>
-    </div>
-    <div class="comments-block">
-        <div class="container">
-            <h1>Комментарии</h1>
-            <div class="row">
-                <div class="col-xs-12">
-                    <div id="disqus_thread"></div>
-                    <script>
-                        var disqus_config = function () {
-                            this.page.url = '{{ $request->url() }}';
-                            this.page.identifier = '{{ $book->slug }}';
-                        };
-
-                        (function() {
-                            var d = document, s = d.createElement('script');
-                            s.src = '//multiversum.disqus.com/embed.js';
-                            s.setAttribute('data-timestamp', +new Date());
-                            (d.head || d.body).appendChild(s);
-                        })();
-                    </script>
-
-                    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
-                </div>
-            </div>
+<div class="Page Page-light">
+    <div class="Page__content Page-light__content">
+        <div class="Book-expanded">
+            <h1 class="Page__heading Page-light__heading">{{ $book->name }}</h1>
+            <img src="http://placehold.it/200x270" class="Page__image" alt="{{ $book->name }}">
+            <button class="btn buy-button">Купить</button>
+            <div class="Book-expanded__description">{!! $book->description !!}</div>
+            <div class="Book-expanded__date">Дата публикации: {{ $book->published_at->format('m.d.Y') }}</div>
         </div>
     </div>
 </div>
+@include('partials.comments', ['resource' => $book])
 @endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,13 +2,11 @@
 @section("content")
 <div class="Page Page-light">
     <div class="Page__content Page-light__content">
-        <h1 class="Page__heading Page-light__heading">{{ $post->name }}</h1>
-        <div class="Post--expanded">
+        <div class="Post-expanded">
+            <h1 class="Page__heading Page-light__heading">{{ $post->name }}</h1>
             <div class="Post-expanded__date">Дата публикации: {{ $post->published_at->format('m.d.Y') }}</div>
             <img src="http://placehold.it/500x500" class="Post-expanded__image" alt="Статья 1">
-            <div class="Post-expanded__info">
-                <p class="Post-expanded__description">{!! $post->content !!}</p>
-            </div>
+            <div class="Post-expanded__description">{!! $post->content !!}</div>
         </div>
     </div>
 </div>
