9927eca792da8f12a7a9d896780ba639d10ca046
emerge <i.i.babko@gmail.com>
2016-03-25 12:26:34 +0200

    fix footer responsiveness

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,15 +3,17 @@
         <div>
             <i class="fa fa-home footer__icon"></i>Наше расположение
         </div>
-  <!--       <ul class="Social">
+        <ul class="Social">
             <li class="Social__item"><i class="fa fa-facebook Social__icon"></i></li>
             <li class="Social__item"><i class="fa fa-instagram Social__icon"></i></li>
             <li class="Social__item"><i class="fa fa-youtube Social__icon"></i></li>
             <li class="Social__item"><i class="fa fa-google-plus Social__icon"></i></li>
-        </ul> -->
+        </ul>
         <div>
-            <a class="logo" href="/">Multiversum</a><br>
-            <span class="footer__copyright">&copy; Все права защищены</span>
+            <div>
+                <a class="logo" href="/">Multiversum</a><br>
+                <span class="footer__copyright">&copy; Все права защищены</span>
+            </div>
         </div>
     </div>
 </footer>
