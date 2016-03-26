b2163f8a48eac752cc87b9c0ff74b30ee31e4bfe
emerge <i.i.babko@gmail.com>
2016-03-26 21:35:59 +0200

    make social links clickable

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,10 +4,9 @@
             <i class="fa fa-home footer__icon"></i>Наше расположение
         </div>
         <ul class="Social">
-            <li class="Social__item"><i class="fa fa-facebook Social__icon"></i></li>
-            <li class="Social__item"><i class="fa fa-instagram Social__icon"></i></li>
-            <li class="Social__item"><i class="fa fa-youtube Social__icon"></i></li>
-            <li class="Social__item"><i class="fa fa-google-plus Social__icon"></i></li>
+            <li class="Social__item"><a class="Social__link" href="https://www.facebook.com/profile.php?id=100006293505911&fref=nf" target="_blank"><i class="fa fa-facebook Social__icon"></i></a></li>
+            <li class="Social__item"><a class="Social__link" href="https://www.youtube.com/channel/UCZuPMVsytT-L-HVTAquUaCA" target="_blank"><i class="fa fa-youtube Social__icon"></i></a></li>
+            <li class="Social__item"><a class="Social__link" href="https://plus.google.com/109801612189366894181" target="_blank"><i class="fa fa-google-plus Social__icon"></i></a></li>
         </ul>
         <div>
             <div>
