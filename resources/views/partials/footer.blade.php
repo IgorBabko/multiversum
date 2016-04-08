87f83add558825c1bdc62cd4adfcf23ec02cc710
emerge <i.i.babko@gmail.com>
2016-04-08 16:22:06 +0300

    style subscription form

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="footer__content">
         <div>
             <!-- <i class="fa fa-home footer__icon"></i>Наше расположение -->
-            <input type="email" class="Input Input--subscripion" placeholder="Ваш email"><button class="Button">подписаться</button>
+            <input type="email" class="Input Input--subscription" placeholder="Ваш email"><button class="Button Button--subscription">подписаться</button>
         </div>
         <ul class="Social">
             <li class="Social__item"><a class="Social__link" href="https://www.facebook.com/profile.php?id=100006293505911&fref=nf" target="_blank"><i class="fa fa-facebook Social__icon"></i></a></li>
