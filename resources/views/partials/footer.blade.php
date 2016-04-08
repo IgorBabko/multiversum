ba38a86ea756cc9e65c31d1bd442a70ba906513d
emerge <i.i.babko@gmail.com>
2016-04-08 16:08:23 +0300

    add subscription field to the footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,8 @@
 <footer class="footer">
     <div class="footer__content">
         <div>
-            <i class="fa fa-home footer__icon"></i>Наше расположение
+            <!-- <i class="fa fa-home footer__icon"></i>Наше расположение -->
+            <input type="email" class="Input Input--subscripion" placeholder="Ваш email"><button class="Button">подписаться</button>
         </div>
         <ul class="Social">
             <li class="Social__item"><a class="Social__link" href="https://www.facebook.com/profile.php?id=100006293505911&fref=nf" target="_blank"><i class="fa fa-facebook Social__icon"></i></a></li>
