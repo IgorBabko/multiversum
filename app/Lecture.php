cefa1d7ba99cad94c168f8ae2d0076cd563268d5
emerge <i.i.babko@gmail.com>
2016-04-16 14:44:08 +0300

    add rate method to Post model

--- app/Lecture.php
+++ app/Lecture.php
@@ -3,10 +3,21 @@
 namespace Multiversum;
 
 use Illuminate\Database\Eloquent\Model;
-use willvincent\Rateable\Rateable;
-use Exception;
+use willvincent\Rateable\Rating;
+use Auth;
 
 class Lecture extends Model
 {
     use Rateable;
+
+    public function rate($rating)
+    {
+        $rating = new Rating;
+
+        $rating->rating = $rating;
+        $rating->user_id = Auth::id();
+
+        $post->ratings()->save($rating);
+    }
+    
 }
