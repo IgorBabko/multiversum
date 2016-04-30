b81df8425c66c032ef9406e8e2d1b2bf42bece1d
emerge <i.i.babko@gmail.com>
2016-04-30 11:27:22 +0300

    clean gulpfile.js and improve styles

--- gulpfile.js
+++ gulpfile.js
@@ -7,41 +7,24 @@ require('laravel-elixir-stylus');
 
 elixir(function(mix) {
 
-    mix.stylus('app.styl', null, {
-        use: [
-            postStylus( [ 'autoprefixer', 'lost', 'rucksack-css' ] )
-        ]
-    });
-
-    // mix.stylus('dashboard.styl', null, {
-    //     use: [
-    //         postStylus( [ 'autoprefixer', 'lost', 'rucksack-css' ] )
-    //     ]
-    // });
-
-    mix.copy('bower_components/oh-snap/ohsnap.min.js', 'public/js/ohsnap.min.js');
-
-    mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/jquery.min.js');
-
-    mix.copy('node_modules/bootstrap-sass/assets/fonts/bootstrap', 'public/fonts/bootstrap');
-    mix.copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css');
-    mix.copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'public/js/bootstrap.min.js');
-    mix.copy('node_modules/font-awesome/css/font-awesome.css', 'public/css/font-awesome.css');
-    mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
-
-    mix.copy('node_modules/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js', 'public/js/ScrollMagic.js');
-    mix.copy('node_modules/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js', 'public/js/debug.addIndicators.js');
-
-    // mix.copy('node_modules/bootstrap-select/dist/css/bootstrap-select.min.css', 'public/css/bootstrap-select.min.css');
-    // mix.copy('node_modules/bootstrap-select/dist/js/bootstrap-select.min.js', 'public/js/bootstrap-select.min.js');
-
-    // dashboard specific files
-    // mix.copy('node_modules/bootstrap-table/dist/bootstrap-table.min.css', 'public/css/bootstrap-table.min.css');
-    // mix.copy('node_modules/bootstrap-table/dist/bootstrap-table.min.js', 'public/js/bootstrap-table.min.js');
-    // mix.copy('node_modules/bootstrap-table/dist/locale/bootstrap-table-ru-RU.min.js', 'public/js/bootstrap-table-ru-RU.min.js');
-
     mix
+        .stylus('app.styl', null, {
+            use: [
+                postStylus( [ 'autoprefixer', 'lost', 'rucksack-css' ] )
+            ]
+        })
         .coffee('app.coffee', 'resources/assets/js')
         .browserify('app.js')
         .livereload();
+
+    mix
+        .copy('bower_components/oh-snap/ohsnap.min.js', 'public/js/ohsnap.min.js')
+        .copy('node_modules/jquery/dist/jquery.min.js', 'public/js/jquery.min.js')
+        .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap', 'public/fonts/bootstrap')
+        .copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css')
+        .copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'public/js/bootstrap.min.js')
+        .copy('node_modules/font-awesome/css/font-awesome.css', 'public/css/font-awesome.css')
+        .copy('node_modules/font-awesome/fonts', 'public/fonts')
+        .copy('node_modules/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js', 'public/js/ScrollMagic.js')
+        .copy('node_modules/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js', 'public/js/debug.addIndicators.js');
 });
