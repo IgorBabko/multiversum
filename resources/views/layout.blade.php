a01fde73854b7aa4e202ed39455a75f93929d26f
emerge <i.i.babko@gmail.com>
2016-05-20 11:01:31 +0300

    and button to layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,6 @@
 <head>
     <title>Multiversum</title>
     <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet"
-    type="text/css">
     <link href="css/app.css" rel="stylesheet" type="text/css">
     @if ( Config::get('app.debug') )
         <script type="text/javascript">
@@ -24,11 +22,12 @@
                     Добро пожаловать на курс:<br>
                     Ментально-духовное развитие
                 </h1>
+                <a class="button button-primary" href="#">ознакомиться</a>
             </div>
             <div class="six columns">
-                <video width="640" height="480" controls>
+                <video width="550" height="420" controls>
                     <source src="https://s3.eu-central-1.amazonaws.com/multiversum/%D0%B2%D1%81%D1%82%D1%83%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5.mp4" type="video/mp4">
-                        Ваш браузер не поддерживает воспроизведение видео
+                    Ваш браузер не поддерживает воспроизведение видео
                 </video>
             </div>
         </div>
