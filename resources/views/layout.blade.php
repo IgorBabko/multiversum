bc75a1a4826653e8f8dd7354314fd2baae7db630
emerge <i.i.babko@gmail.com>
2016-05-20 10:25:27 +0300

    add content to layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,8 +11,14 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="one column">One</div>
-            <div class="eleven columns">Eleven</div>
+            <div class="six columns"><h4 style="font-family: 'Lato'; font-weight: 100">Добро пожаловать на курс ментально духовного развития - Multiversum</h1></div>
+            <div class="six columns">
+                <video width="320" height="240" controls>
+                    <source src="movie.mp4" type="video/mp4">
+                    <source src="movie.ogg" type="video/ogg">
+                        Ваш браузер не поддерживает воспроизведение видео
+                </video>
+            </div>
         </div>
     </div>
 </body>
