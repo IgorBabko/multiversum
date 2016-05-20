8814e3d4f4c4f39ca3ef50cd7bdbdbea4045ecac
emerge <i.i.babko@gmail.com>
2016-05-20 10:10:36 +0300

    create HomeController.php

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Multiversum</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet"
+    type="text/css">
+    <link href="css/app.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="one column">One</div>
+            <div class="eleven columns">Eleven</div>
+        </div>
+    </div>
+</body>
+
+</html>
