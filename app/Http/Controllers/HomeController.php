3cb46704dfd6f5d15d5207bf78695b84f8d85ea4
emerge <i.i.babko@gmail.com>
2016-05-26 11:21:27 +0300

    fire php artisan make:auth command

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,28 @@
 
 namespace Multiversum\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use Multiversum\Http\Requests;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        return view('layout');
-    } 
+        return view('home');
+    }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,3 +12,7 @@
 */
 
 Route::get('/', 'HomeController@index');
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
