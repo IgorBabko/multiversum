c8a47487f97520678cc3114d1fe78c39ce90eb55
emerge <i.i.babko@gmail.com>
2016-05-30 10:39:14 +0300

    add liqpay payment button

--- resources/views/sections/payment.blade.php
+++ resources/views/sections/payment.blade.php
@@ -3,8 +3,8 @@
     <div class="container">
         <h2>Оплата</h2>
         @if ( !Auth::user() )
-            <div class="row">Для получения полного доступа к курсу пройдите простую регистрацию ниже
-                и оплатите курс всего за <span class="price">10 грн<span>
+        <div class="row">Для получения полного доступа к курсу пройдите простую регистрацию ниже
+            и оплатите курс всего за <span class="price">10 грн<span>
             </div>
             <div class="row account-buttons">
                 <a class="register-button link-button" href="#register">Регистрация</a>
@@ -13,7 +13,12 @@
         @else
             <div class="row">Для получения полного курса внесите оплату в размере <span class="price">10 грн<span></div>
             <div class="row account-buttons">
-                <a class="payment-button link-button" href="/payment">Оплатить</a>
+                <form method="POST" accept-charset="utf-8" action="https://www.liqpay.com/api/3/checkout">
+                    <input type="hidden" name="data" value="eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXkiLCJwdWJsaWNfa2V5IjoiaTMwNzcxOTUzNDA0IiwiYW1vdW50IjoiMTAiLCJjdXJyZW5jeSI6IlVBSCIsImRlc2NyaXB0aW9uIjoi0JrRg9GA0YEg0LvQtdC60YbQuNC5IiwidHlwZSI6ImJ1eSIsInNlcnZlcl91cmwiOiJodHRwOi8vbXVsdGl2ZXJzdW0ubWwiLCJsYW5ndWFnZSI6InJ1In0=" />
+                    <input type="hidden" name="signature" value="1dO4H/6f0D17m7DlPQFP4xmvNbE=" />
+                    <input type="image" src="//static.liqpay.com/buttons/p1ru.radius.png" name="btn_text" />
+                </form>
+                <!--<a class="payment-button link-button" href="/payment">Оплатить</a>-->
             </div>
         @endif
     </div>
