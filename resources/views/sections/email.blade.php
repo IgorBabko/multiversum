f6c18a81d4821c0c4add1dc45531db878577546a
emerge <i.i.babko@gmail.com>
2016-05-30 10:28:02 +0300

    display email in email section of the email page

--- resources/views/sections/email.blade.php
+++ resources/views/sections/email.blade.php
@@ -1,6 +1,7 @@
 <div class="section" id="email-section">
     <div class="container centered">
         <h2 class="email-heading">Свяжитесь со мной</h2> 
+        i.i.babko@gmail.com 
         <img src="/img/mail.jpg" class="mail-img">
         <a class="email-button link-button" href="#email">Написать</a>
     </div>
--- resources/views/sections/main.blade.php
+++ resources/views/sections/main.blade.php
@@ -5,8 +5,16 @@
                 <h2 class="main-heading">Ментально-духовное развитие </h2>
             </div>
             <div>
-                <video class="u-max-full-width" style="margin: 30px 0" width="640" height="480" controls>
-                    <source src="https://s3.eu-central-1.amazonaws.com/multiversum/%d0%b2%d1%81%d1%82%d1%83%d0%bf%d0%bb%d0%b5%d0%bd%d0%b8%d0%b5.mp4" type="video/mp4">ваш браузер не поддерживает воспроизведение видео</video>
+            <video id="my-video" class="video-js" controls preload="auto" width="640" height="480"
+            poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
+                <source src="https://s3.eu-central-1.amazonaws.com/multiversum/%d0%b2%d1%81%d1%82%d1%83%d0%bf%d0%bb%d0%b5%d0%bd%d0%b8%d0%b5.mp4" type='video/mp4'>
+                    <source src="MY_VIDEO.webm" type='video/webm'>
+                        <p class="vjs-no-js">
+                            Для просмотра этого видео, пожалуйста, включите поддержку JavaScript и, желательно,
+                            обновите Ваш браузер
+                            <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+                        </p>
+            </video>
             </div>
             <div>
                 <a class="main-button link-button">ознакомиться</a>
