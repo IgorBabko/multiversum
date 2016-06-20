636eb10a1c7b5788e0fec135e2acb743f4a17fe7
emerge <i.i.babko@gmail.com>
2016-06-20 17:54:16 +0300

    small syntax fix

--- resources/views/sections/payment.blade.php
+++ resources/views/sections/payment.blade.php
@@ -16,10 +16,10 @@
             Для получения полного курса внесите оплату в размере <span class="price">10 грн<span>
         </div>
         <div class="row account-buttons">
-            {{ $unique_id = uniqid('php_'); }}
+            {{ $unique_id = uniqid('php_') }}
             <form method="POST" accept-charset="utf-8" action="https://www.liqpay.com/api/3/checkout">
-                <input type="hidden" name="data" value='{{ base64_encode("{\"result_url\": \"http://multiversum.ml", \"server_url\": \"http://multiversum.ml/payment\", \"order_id\": \"" . $unique_id . "\", \"description\": \"" . Auth::user()->email . "\", \"amount\": 2, \"currency\": \"UAH\", \"action\": \"pay\", \"public_key\": \"i20047806537\"}") }}' />
-                <input type="hidden" name="signature" value='{{ base64_encode( sha1( env('LIQPAY_PRIVATE_KEY') . base64_encode("{\"result_url\": \"http://multiversum.ml", \"server_url\": \"http://multiversum.ml/payment\", \"order_id\": \"" . $unique_id . "\", \"description\": \"" . Auth::user()->email . "\", \"amount\": 2, \"currency\": \"UAH\", \"action\": \"pay\", \"public_key\": \"i20047806537\"}") . env('LIQPAY_PRIVATE_KEY'), 1 ) ) }}' />
+                <input type="hidden" name="data" value='{{ base64_encode("{\"result_url\": \"http://multiversum.ml\", \"server_url\": \"http://multiversum.ml/payment\", \"order_id\": \"" . $unique_id . "\", \"description\": \"" . Auth::user()->email . "\", \"amount\": 2, \"currency\": \"UAH\", \"action\": \"pay\", \"public_key\": \"i20047806537\"}") }}' />
+                <input type="hidden" name="signature" value='{{ base64_encode( sha1( env('LIQPAY_PRIVATE_KEY') . base64_encode("{\"result_url\": \"http://multiversum.ml\", \"server_url\": \"http://multiversum.ml/payment\", \"order_id\": \"" . $unique_id . "\", \"description\": \"" . Auth::user()->email . "\", \"amount\": 2, \"currency\": \"UAH\", \"action\": \"pay\", \"public_key\": \"i20047806537\"}") . env('LIQPAY_PRIVATE_KEY'), 1 ) ) }}' />
                 <input type="image" src="//static.liqpay.com/buttons/p1ru.radius.png" name="btn_text" />
             </form>
         </div>
