376b312507f8be4b17d5f8c14451ac734ec9f96d
emerge <i.i.babko@gmail.com>
2016-06-24 10:30:56 +0300

    improve design

--- resources/views/sections/main.blade.php
+++ resources/views/sections/main.blade.php
@@ -1,24 +1,27 @@
-<div class="container section-main">
-    <main class="row section" style="padding-top: 20px" id="promo-section">
-        <div>
+<div class="section section-main" id="about-section">
+    <div class="container">
+        <main class="row section" style="padding-top: 20px" id="promo-section">
             <div>
-                <h2 class="main-heading">Ментально-духовное развитие </h2>
+                <div>
+                    <h2 class="main-heading" style="color: beige">Ментально-духовное развитие </h2>
+                </div>
+                <div>
+                    <video id="my-video" class="video-js" controls preload="auto" width="640" height="480"
+                    poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
+                        <source src="https://s3.eu-central-1.amazonaws.com/multiversum/%d0%b2%d1%81%d1%82%d1%83%d0%bf%d0%bb%d0%b5%d0%bd%d0%b8%d0%b5.mp4"
+                        type='video/mp4'>
+                            <source src="MY_VIDEO.webm" type='video/webm'>
+                                <p class="vjs-no-js">
+                                    Для просмотра этого видео, пожалуйста, включите поддержку JavaScript и, желательно,
+                                    обновите Ваш браузер
+                                    <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+                                </p>
+                    </video>
+                </div>
+                <div>
+                    <a class="main-button link-button">ознакомиться</a>
+                </div>
             </div>
-            <div>
-            <video id="my-video" class="video-js" controls preload="auto" width="640" height="480"
-            poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
-                <source src="https://s3.eu-central-1.amazonaws.com/multiversum/%d0%b2%d1%81%d1%82%d1%83%d0%bf%d0%bb%d0%b5%d0%bd%d0%b8%d0%b5.mp4" type='video/mp4'>
-                    <source src="MY_VIDEO.webm" type='video/webm'>
-                        <p class="vjs-no-js">
-                            Для просмотра этого видео, пожалуйста, включите поддержку JavaScript и, желательно,
-                            обновите Ваш браузер
-                            <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-                        </p>
-            </video>
-            </div>
-            <div>
-                <a class="main-button link-button">ознакомиться</a>
-            </div>
-        </div>
-    </main>
+        </main>
+    </div>
 </div>
