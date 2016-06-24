70a408e7de40b7f4a5a8aa2cf70c057fa65100d2
emerge <i.i.babko@gmail.com>
2016-06-24 18:51:41 +0300

    change main section and add picture with quote to it

--- resources/views/sections/main.blade.php
+++ resources/views/sections/main.blade.php
@@ -1,11 +1,11 @@
 <div class="section section-main" id="about-section">
-    <div class="container">
-        <main class="row section" style="padding-top: 20px" id="promo-section">
+    <div>
+        <main class="row" id="promo-section">
             <div>
                 <div>
                     <h2 class="main-heading" style="color: beige">Ментально-духовное развитие </h2>
                 </div>
-                <div>
+                <div style="display: flex; justify-content: center; align-items: center">
                     <video id="my-video" class="video-js" controls preload="auto" width="640" height="480"
                     poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
                         <source src="https://s3.eu-central-1.amazonaws.com/multiversum/%d0%b2%d1%81%d1%82%d1%83%d0%bf%d0%bb%d0%b5%d0%bd%d0%b8%d0%b5.mp4"
@@ -17,9 +17,9 @@
                                     <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
                                 </p>
                     </video>
-                </div>
-                <div>
-                    <a class="main-button link-button">ознакомиться</a>
+                    <div>
+                        <img style="margin-left: 50px" src="img/quote.jpg">
+                    </div>
                 </div>
             </div>
         </main>
