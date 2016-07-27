026eae121e3dacf7097dabcbbd9276594bc562ae
Igor Babko <i.i.babko@gmail.com>
2016-07-27 17:55:44 +0300

    change about section

--- resources/views/sections/main.blade.php
+++ resources/views/sections/main.blade.php
@@ -1,29 +1,38 @@
 <div class="section section-main" id="about-section">
     <div>
         <main class="row" id="promo-section">
+        <div>
             <div>
-                <div>
-                    <h3 style="color: white; text-align: center">О чем этот курс?</h3>
-                </div>
-                <div style="display: flex; justify-content: center; align-items: center">
-                    <div class="main-video-wrapper">
-                        <!--<h3 class="main-heading subheading" style="text-align: center; margin-bottom: 10px; color: beige">О курсе</h3>-->
-                        <video id="my-video" class="video-js" controls preload="auto" width="640" height="480"
+                <h3 style="color: white; text-align: center">О чем этот курс?</h3>
+            </div>
+            <div style="display: flex; justify-content: center; align-items: center">
+                <div class="main-video-wrapper">
+                    <!--<h3 class="main-heading subheading" style="text-align: center; margin-bottom: 10px; color: beige">О курсе</h3>-->
+                    <video id="my-video" class="video-js" controls preload="auto" width="640" height="480"
                         poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
-                            <source src="https://s3.eu-central-1.amazonaws.com/multiversum/%D0%B2%D1%81%D1%82%D1%83%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5+%D0%BA+%D0%BA%D1%83%D1%80%D1%81%D1%83+%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE-%D0%B4%D1%83%D1%85%D0%BE%D0%B2%D0%BD%D0%BE%D0%B3%D0%BE+%D1%80%D0%B0%D0%B7%D0%B2%D0%B8%D1%82%D0%B8%D1%8F.mp4"
-                            type='video/mp4'>
-                                <source src="MY_VIDEO.webm" type='video/webm'>
-                                    <p class="vjs-no-js">
-                                        Для просмотра этого видео, пожалуйста, включите поддержку JavaScript и, желательно,
-                                        обновите Ваш браузер
-                                        <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-                                    </p>
-                        </video>
-                    </div>
-                    <div style="display: flex; flex-direction: column; justify-content: center">
-                        <img src="img/quote.jpg">
-                    </div>
+                        <source src="https://s3.eu-central-1.amazonaws.com/multiversum/%D0%B2%D1%81%D1%82%D1%83%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5+%D0%BA+%D0%BA%D1%83%D1%80%D1%81%D1%83+%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE-%D0%B4%D1%83%D1%85%D0%BE%D0%B2%D0%BD%D0%BE%D0%B3%D0%BE+%D1%80%D0%B0%D0%B7%D0%B2%D0%B8%D1%82%D0%B8%D1%8F.mp4"
+                        type='video/mp4'>
+                        <source src="MY_VIDEO.webm" type='video/webm'>
+                        <p class="vjs-no-js">
+                            Для просмотра этого видео, пожалуйста, включите поддержку JavaScript и, желательно,
+                            обновите Ваш браузер
+                            <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+                        </p>
+                    </video>
                 </div>
+                <div style="display: flex; flex-direction: column; justify-content: center">
+                    <img src="img/quote.jpg">
+                </div>
+            </div>
+        </div>
         </main>
+        <div class="container">
+            <div class="row">
+                <div class="twelve columns about-course">
+                    <p>Новинка! Ментально-духовное развитие. В чем смысл жизни? В чем истина? Что такое судьба?</p>
+                    <p>Курс по ментально-духовному развитию,  философский курс ,  реинкарнация, левитация, телепортация, учение о реинкарнации, тело и душа, духовность, культура, причины болезней, судьба, философия судьбы, любовь, философия любви, дуализм,  здоровый образ жизни, здоровый человек,  философия смерти, земля, планета Земля,  кем я был и кем я буду, процесс познания, человеческий мозг, нейроны мозга,  метафизика,  теория струн, квантовая механика, Николо Тесла, Роберт Бартини, Лев Толстой, смысл инь-янь, информация суть,   Бог суть, нейроны, мозг человека, информация, дух, душа, культура, страх, психология, египтяне.</p>
+                </div>
+            </div>
         </div>
     </div>
+</div>
